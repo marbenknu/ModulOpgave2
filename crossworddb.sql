d20776eb793c7bb29d5750f8f90ef4f2b1a57274
efjohannsen <efjohannsen@gmail.com>
2019-03-16 16:24:20 +0100

    SQL kører nu på 2 min

--- crossworddb.sql
+++ crossworddb.sql
@@ -1,17 +1,19 @@
 -- CREATE DATABASE AND TABLE
-DROP DATABASE IF EXISTS crossword;
-CREATE DATABASE IF NOT EXISTS crossword;
-USE crossword;
 DROP TABLE IF EXISTS wordlist;
-CREATE TABLE IF NOT EXISTS wordlist
+DROP DATABASE IF EXISTS crossword;
+CREATE DATABASE crossword;
+USE crossword;
+CREATE TABLE wordlist
 (
 	words	CHAR(3)	NOT NULL
-) CHARACTER SET utf8 COLLATE utf8_danish_ci;
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_danish_ci;
 
 -- LOAD DATAFILE
-LOAD DATA INFILE "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\3LetterWord_CSV.csv"-- 3LetterWord_CSV_v2.csv"
+LOAD DATA INFILE "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\renset.txt"-- 3LetterWord_CSV.csv"
 INTO TABLE wordlist
-CHARACTER SET latin1;
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ';' ENCLOSED BY '' ESCAPED BY '\\'
+IGNORE 1 LINES;
 
 -- CREATE USER
 DROP USER IF EXISTS 'moduluser'@'localhost';
@@ -30,16 +32,19 @@ SUBSTR(ta.words,1,1) = SUBSTR(tb.words,1,1)
 && SUBSTR(ta.words,3,1) != SUBSTR(tb.words,2,1) && SUBSTR(ta.words,3,1) != SUBSTR(tb.words,3,1)
 -- c join a
 && SUBSTR(ta.words,2,1) = SUBSTR(tc.words,1,1)
+-- test v1 for eksistens af e og f
+&& CONCAT(SUBSTR(tb.words,2,1),SUBSTR(tc.words,2,1)) IN (SELECT SUBSTR(words,1,2) AS temp FROM wordlist)
+&& CONCAT(SUBSTR(tb.words,3,1),SUBSTR(tc.words,3,1)) IN (SELECT SUBSTR(words,1,2) AS temp FROM wordlist)
 -- a, b og c indeholder unikke bogstaver
 && SUBSTR(tc.words,2,1) != SUBSTR(ta.words,1,1) && SUBSTR(tc.words,2,1) != SUBSTR(ta.words,3,1)
 && SUBSTR(tc.words,2,1) != SUBSTR(tb.words,2,1) && SUBSTR(tc.words,2,1) != SUBSTR(tb.words,3,1)
 && SUBSTR(tc.words,3,1) != SUBSTR(ta.words,1,1) && SUBSTR(tc.words,3,1) != SUBSTR(ta.words,3,1)
 && SUBSTR(tc.words,3,1) != SUBSTR(tb.words,2,1) && SUBSTR(tc.words,3,1) != SUBSTR(tb.words,3,1)
--- test v1 for eksistens af e og f
-&& CONCAT(SUBSTR(tb.words,2,1),SUBSTR(tc.words,2,1)) IN (SELECT CONCAT(SUBSTR(words,1,1),SUBSTR(words,2,1)) AS temp FROM wordlist)
-&& CONCAT(SUBSTR(tb.words,3,1),SUBSTR(tc.words,3,1)) IN (SELECT CONCAT(SUBSTR(words,1,1),SUBSTR(words,2,1)) AS temp FROM wordlist)
 -- d join a
 && SUBSTR(ta.words,3,1) = SUBSTR(td.words,1,1)
+-- test v2 for eksistens af e og f
+&& CONCAT(SUBSTR(tb.words,2,1),SUBSTR(tc.words,2,1),SUBSTR(td.words,2,1)) IN (SELECT words FROM wordlist)
+&& CONCAT(SUBSTR(tb.words,3,1),SUBSTR(tc.words,3,1),SUBSTR(td.words,3,1)) IN (SELECT words FROM wordlist)
 -- a, b, c og d indeholder unikke bogstaver
 && SUBSTR(td.words,2,1) != SUBSTR(ta.words,1,1) && SUBSTR(td.words,2,1) != SUBSTR(ta.words,2,1)
 && SUBSTR(td.words,2,1) != SUBSTR(tb.words,2,1) && SUBSTR(td.words,2,1) != SUBSTR(tb.words,3,1)
@@ -47,7 +52,4 @@ SUBSTR(ta.words,1,1) = SUBSTR(tb.words,1,1)
 && SUBSTR(td.words,3,1) != SUBSTR(ta.words,1,1) && SUBSTR(td.words,3,1) != SUBSTR(ta.words,2,1)
 && SUBSTR(td.words,3,1) != SUBSTR(tb.words,2,1) && SUBSTR(td.words,3,1) != SUBSTR(tb.words,3,1)
 && SUBSTR(td.words,3,1) != SUBSTR(tc.words,2,1) && SUBSTR(td.words,3,1) != SUBSTR(tc.words,3,1)
--- test v2 for eksistens af e og f
-&& CONCAT(SUBSTR(tb.words,2,1),SUBSTR(tc.words,2,1),SUBSTR(td.words,2,1)) IN (SELECT words FROM wordlist)
-&& CONCAT(SUBSTR(tb.words,3,1),SUBSTR(tc.words,3,1),SUBSTR(td.words,3,1)) IN (SELECT words FROM wordlist)
-ORDER BY ta.words ASC, tb.words ASC, tc.words ASC, td.words ASC;
+;-- ORDER BY ta.words ASC, tb.words ASC, tc.words ASC, td.words ASC;
