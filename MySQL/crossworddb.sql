134e83b2f6223c29b87cbb5c17a415b9f0024d49
efjohannsen <efjohannsen@gmail.com>
2019-03-24 15:41:50 +0100

    cleanup

--- MySQL/crossworddb.sql
+++ MySQL/crossworddb.sql
@@ -3,8 +3,8 @@ DROP DATABASE IF EXISTS crossword;
 CREATE DATABASE IF NOT EXISTS crossword;
 USE crossword;
 CREATE TABLE wordlist (
-    words CHAR(3) NOT NULL
-)  CHARACTER SET UTF8MB4 COLLATE UTF8MB4_DANISH_CI;
+	words CHAR(3) NOT NULL
+) CHARACTER SET UTF8MB4 COLLATE UTF8MB4_DANISH_CI;
 
 -- LOAD DATAFILE
 LOAD DATA INFILE "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\3LetterWord_Unicode_NOBOM_UTF8.txt"
@@ -18,3 +18,9 @@ DROP USER IF EXISTS 'moduluser'@'localhost';
 CREATE USER IF NOT EXISTS 'moduluser'@'localhost' IDENTIFIED BY 'gruppe7';
 GRANT ALL ON *.* TO 'moduluser'@'localhost';
 FLUSH PRIVILEGES;-- Only needed when modifying the grant tables directly using statements such as INSERT, UPDATE, or DELETE
+
+/*
+SET @@global.time_zone = '+00:00';
+SET @@session.time_zone = '+00:00';
+SELECT @@global.time_zone, @@session.time_zone;
+*/
